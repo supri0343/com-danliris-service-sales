24001ffbe1dde36d79cce4614e2f3ba35d92beb3
kuswandanu <kuswandanu@gmail.com>
2020-04-06 09:09:50 +0700

    CostCalculationGarment MaterialIndex

--- Com.Danliris.Service.Sales.Lib/Migrations/20200406015023_CostCalculationGarment_MaterialIndex.Designer.cs
+++ Com.Danliris.Service.Sales.Lib/Migrations/20200406015023_CostCalculationGarment_MaterialIndex.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Sales.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Sales.Lib.Migrations
 {
     [DbContext(typeof(SalesDbContext))]
-    partial class SalesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200406015023_CostCalculationGarment_MaterialIndex")]
+    partial class CostCalculationGarment_MaterialIndex
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
