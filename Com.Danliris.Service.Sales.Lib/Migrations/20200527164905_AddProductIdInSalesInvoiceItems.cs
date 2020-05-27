a5b7dc5469921a70bd83f291a0f67eb1f3f94754
Franciskus Napitupulu <8franciskusna70@gmail.com>
2020-05-27 23:56:29 +0700

    AddMigrationAddProductId

---
+++ Com.Danliris.Service.Sales.Lib/Migrations/20200527164905_AddProductIdInSalesInvoiceItems.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Sales.Lib.Migrations
+{
+    public partial class AddProductIdInSalesInvoiceItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ProductId",
+                table: "SalesInvoiceItems",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductId",
+                table: "SalesInvoiceItems");
+        }
+    }
+}
