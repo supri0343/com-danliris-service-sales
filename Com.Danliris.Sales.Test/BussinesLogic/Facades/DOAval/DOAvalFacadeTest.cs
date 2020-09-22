6800060503913f17fb73abea5339f2e7810196e5
skydivine <17161989+skydivine@users.noreply.github.com>
2020-09-22 15:05:05 +0700

    Add DO Aval

---
+++ Com.Danliris.Sales.Test/BussinesLogic/Facades/DOAval/DOAvalFacadeTest.cs
@@ -0,0 +1,23 @@
+﻿using Com.Danliris.Sales.Test.BussinesLogic.DataUtils.DOAval;
+using Com.Danliris.Sales.Test.BussinesLogic.DataUtils.DOSales;
+using Com.Danliris.Sales.Test.BussinesLogic.Utils;
+using Com.Danliris.Service.Sales.Lib;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.DOAval;
+using Com.Danliris.Service.Sales.Lib.BusinessLogic.Logic.DOAval;
+using Com.Danliris.Service.Sales.Lib.Models.DOSales;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Sales.Test.BussinesLogic.Facades.DOAval
+{
+    public class DOAvalFacadeTest : BaseFacadeTest<SalesDbContext, DOAvalFacade, DOAvalLogic, DOSalesModel, DOAvalDataUtil>
+    {
+        private const string ENTITY = "DOAval";
+        public DOAvalFacadeTest() : base(ENTITY)
+        {
+        }
+
+
+    }
+}
---
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Interface/DOAval/IDOAvalFacade.cs
@@ -0,0 +1,12 @@
+﻿using Com.Danliris.Service.Sales.Lib.Models.DOSales;
+using Com.Danliris.Service.Sales.Lib.Utilities.BaseInterface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Interface.DOAval
+{
+    public interface IDOAvalFacade : IBaseFacade<DOSalesModel>
+    {
+    }
+}
