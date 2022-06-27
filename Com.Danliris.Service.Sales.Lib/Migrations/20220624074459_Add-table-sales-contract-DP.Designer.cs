cc80484b09d5eb88564fc7a91f61f19338408a7c
Joifarflex <joifarjosuajosua@gmail.com>
2022-06-27 11:48:29 +0700

    update pdf sc
    update pdf sc dying printing
    update pdf sc weaving
    update pdf sc spinning

--- Com.Danliris.Service.Sales.Lib/Migrations/20220624074459_Add-table-sales-contract-DP.Designer.cs
+++ Com.Danliris.Service.Sales.Lib/Migrations/20220624074459_Add-table-sales-contract-DP.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Sales.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Sales.Lib.Migrations
 {
     [DbContext(typeof(SalesDbContext))]
-    partial class SalesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220624074459_Add-table-sales-contract-DP")]
+    partial class AddtablesalescontractDP
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -4221,8 +4223,6 @@ namespace Com.Danliris.Service.Sales.Lib.Migrations
                     b.Property<string>("BuyerType")
                         .HasMaxLength(255);
 
-                    b.Property<double?>("Claim");
-
                     b.Property<string>("Code")
                         .HasMaxLength(255);
 
@@ -4293,10 +4293,6 @@ namespace Com.Danliris.Service.Sales.Lib.Migrations
                         .HasMaxLength(255);
 
                     b.Property<DateTime>("LastModifiedUtc");
-
-                    b.Property<int>("LatePayment");
-
-                    b.Property<int>("LateReturn");
 
                     b.Property<string>("MaterialCode")
                         .HasMaxLength(25);
@@ -4441,8 +4437,6 @@ namespace Com.Danliris.Service.Sales.Lib.Migrations
                     b.Property<string>("BuyerType")
                         .HasMaxLength(255);
 
-                    b.Property<double?>("Claim");
-
                     b.Property<string>("Code")
                         .HasMaxLength(255);
 
@@ -4513,10 +4507,6 @@ namespace Com.Danliris.Service.Sales.Lib.Migrations
                         .HasMaxLength(255);
 
                     b.Property<DateTime>("LastModifiedUtc");
-
-                    b.Property<int>("LatePayment");
-
-                    b.Property<int>("LateReturn");
 
                     b.Property<string>("MaterialCode")
                         .HasMaxLength(25);
--- Com.Danliris.Service.Sales.Lib/Models/FinishingPrinting/FinishingPrintingSalesContractModel.cs
+++ Com.Danliris.Service.Sales.Lib/Models/FinishingPrinting/FinishingPrintingSalesContractModel.cs
@@ -70,6 +70,7 @@ namespace Com.Danliris.Service.Sales.Lib.Models.FinishingPrinting
         public string BuyerName { get; set; }
         [MaxLength(255)]
         public string BuyerType { get; set; }
+        public string BuyerJob { get; set; }
         #endregion
         [MaxLength(25)]
         public string Code { get; set; }
@@ -174,6 +175,23 @@ namespace Com.Danliris.Service.Sales.Lib.Models.FinishingPrinting
         [MaxLength(255)]
         public string YarnMaterialName { get; set; }
         #endregion
+
+        #region Product Type
+        public int ProductTypeId { get; set; }
+        [MaxLength(25)]
+        public string ProductTypeCode { get; set; }
+        [MaxLength(255)]
+        public string ProductTypeName { get; set; }
+        #endregion
+
+        public string DownPayments { get; set; }
+        public double PriceDP { get; set; }
+        public double precentageDP { get; set; }
+        public string PaymentMethods { get; set; }
+        public int Day { get; set; }
+        public int LatePayment { get; set; }
+        public int LateReturn { get; set; }
+        public double? Claim { get; set; }
         public virtual ICollection<FinishingPrintingSalesContractDetailModel> Details { get; set; }
     }
 }
