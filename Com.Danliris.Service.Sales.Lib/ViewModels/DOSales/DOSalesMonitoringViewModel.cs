7c917733ebdaf876ecd32bf6e75ac90b5872ac7c
achensetyawati <achenzetya@gmail.com>
2023-02-20 13:52:23 +0700

    add DOSales Monitoring

---
+++ Com.Danliris.Service.Sales.Lib/ViewModels/DOSales/DOSalesMonitoringViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Com.Danliris.Service.Sales.Lib.ViewModels.DOSales
+{
+    public class DOSalesMonitoringViewModel
+    {
+        public DateTimeOffset Date { get; set; }
+        public string DOSalesNo { get; set; }
+        public string BuyerType { get; set; }
+        public string BuyerName { get; set; }
+        public string ProductionOrderNo { get; set; }
+        public int ProductionOrderId { get; set; }
+
+        
+        public string SalesName { get; set; }
+        public double OrderQuantity { get; set; }
+        public double PackingQty { get; set; }
+        public double StockQty { get; set; }
+    }
+}
---
+++ Com.Danliris.Service.Sales.WebApi/Controllers/DOSalesMonitoringController.cs
@@ -0,0 +1,26 @@
+﻿using Com.Danliris.Service.Sales.Lib.BusinessLogic.Interface.DOSales;
+using Com.Danliris.Service.Sales.Lib.Services;
+using Com.Danliris.Service.Sales.Lib.ViewModels.DOSales;
+using Com.Danliris.Service.Sales.WebApi.Utilities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Com.Danliris.Service.Sales.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [ApiVersion("1.0")]
+    [Route("v{version:apiVersion}/sales/do-sales/monitoring")]
+    [Authorize]
+    public class DOSalesMonitoringController : BaseMonitoringController<DOSalesMonitoringViewModel, IDOSalesMonitoring>
+    {
+        private readonly static string apiVersion = "1.0";
+
+        public DOSalesMonitoringController(IIdentityService identityService, IDOSalesMonitoring facade) : base(identityService, facade, apiVersion)
+        {
+        }
+    }
+}
