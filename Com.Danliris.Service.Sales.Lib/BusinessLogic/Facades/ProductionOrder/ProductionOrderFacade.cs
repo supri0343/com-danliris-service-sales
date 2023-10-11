4b9913b251f5618c3213766d45a219dfb8310f45
UrMama <141613642+Mudipas@users.noreply.github.com>
2023-10-11 09:09:25 +0700

    MDP Bug Fix IEDP Tanggal Delivery.
    Expoer Excel

--- Com.Danliris.Service.Sales.Lib/BusinessLogic/Facades/ProductionOrder/ProductionOrderFacade.cs
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Facades/ProductionOrder/ProductionOrderFacade.cs
@@ -757,6 +757,62 @@ namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Facades.ProductionOrder
             return Query = query.AsQueryable();
         }
 
+        //--CobaMDP--//
+        public async Task<IQueryable<ProductionOrderReportViewModel>> GetReportQuery2(string salesContractNo, string orderNo, string orderTypeId, string processTypeId, string buyerId, string accountId, DateTime? dateFrom, DateTime? dateTo, int offset)
+        {
+            string OrderTypeId1;
+            string OrderTypeId2;
+            if (orderTypeId == "1" || orderTypeId == "9")
+            {
+                OrderTypeId1 = "9";
+                OrderTypeId2 = "1";
+            }
+            else
+            {
+                OrderTypeId1 = orderTypeId;
+                OrderTypeId2 = orderTypeId;
+            }
+
+            DateTime DateFrom = dateFrom == null ? new DateTime(1970, 1, 1) : (DateTime)dateFrom;
+            DateTime DateTo = dateTo == null ? DateTime.Now : (DateTime)dateTo;
+
+            var Query1 = (from a in DbContext.ProductionOrder
+                          join b in DbContext.ProductionOrder_Details on a.Id equals b.ProductionOrderModel.Id
+                          join c in DbContext.FinishingPrintingSalesContracts on a.SalesContractNo equals c.SalesContractNo
+                          join d in DbContext.FinishingPrintingSalesContractDetails on c.Id equals d.FinishingPrintingSalesContract.Id
+                          where a.IsDeleted == false
+                              && b.IsDeleted == false
+                              && a.SalesContractNo == (string.IsNullOrWhiteSpace(salesContractNo) ? a.SalesContractNo : salesContractNo)
+                              && a.CreatedUtc.AddHours(offset).Date >= DateFrom.Date
+                              && a.CreatedUtc.AddHours(offset).Date <= DateTo.Date
+                              && a.OrderNo == (string.IsNullOrWhiteSpace(orderNo) ? a.OrderNo : orderNo)
+                              && a.BuyerId.ToString() == (string.IsNullOrWhiteSpace(buyerId) ? a.BuyerId.ToString() : buyerId)
+                              && (a.OrderTypeId.ToString() == (string.IsNullOrWhiteSpace(OrderTypeId1) ? a.OrderTypeId.ToString() : OrderTypeId1)
+                                  || a.OrderTypeId.ToString() == (string.IsNullOrWhiteSpace(OrderTypeId2) ? a.OrderTypeId.ToString() : OrderTypeId2))
+                              && a.ProcessTypeId.ToString() == (string.IsNullOrWhiteSpace(processTypeId) ? a.ProcessTypeId.ToString() : processTypeId)
+                              && a.AccountId.ToString() == (string.IsNullOrWhiteSpace(accountId) ? a.AccountId.ToString() : accountId)
+                          select new ProductionOrderReportViewModel
+                          {
+                              orderNo = a.OrderNo,
+                              colorType = b.ColorType,
+                              construction = a.MaterialName + " / " + a.MaterialConstructionName + " / " + a.MaterialWidth,
+                              deliveryDate = a.DeliveryDate,
+                              designCode = a.DesignCode,
+                              buyer = a.BuyerName,
+                              _createdDate = a.CreatedUtc,
+                              finishTypeName = a.FinishTypeName,
+                              standardTest= a.StandardTestName,
+                              finishWidth = a.FinishWidth,
+                              materialName = a.MaterialName,
+                              yarnMaterialName = a.YarnMaterialName,
+                              materialWidth = a.MaterialWidth,
+                              handlingStandard = a.HandlingStandard,
+                              orderQuantity = b.Quantity
+                          });
+
+            return Query1;
+        }
+
         public async Task<Tuple<List<ProductionOrderReportViewModel>, int>> GetReport(string salesContractNo, string orderNo, string orderTypeId, string processTypeId, string buyerId, string accountId, DateTime? dateFrom, DateTime? dateTo, int page, int size, string Order, int offset)
         {
             var Query = await GetReportQuery(salesContractNo, orderNo, orderTypeId, processTypeId, buyerId, accountId, dateFrom, dateTo, offset);
