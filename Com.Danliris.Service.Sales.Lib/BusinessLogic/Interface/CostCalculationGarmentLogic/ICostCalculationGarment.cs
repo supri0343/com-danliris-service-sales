20fc0c3e03efdc4f956e970853a59214153f63f6
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-04-16 10:22:58 +0700

    Sprint9-Fix-CalculationGShippingInvoice-Excel

--- Com.Danliris.Service.Sales.Lib/BusinessLogic/Interface/CostCalculationGarmentLogic/ICostCalculationGarment.cs
+++ Com.Danliris.Service.Sales.Lib/BusinessLogic/Interface/CostCalculationGarmentLogic/ICostCalculationGarment.cs
@@ -28,5 +28,6 @@ namespace Com.Danliris.Service.Sales.Lib.BusinessLogic.Interface.CostCalculation
         ReadResponse<dynamic> ReadMaterials(int page, int size, string order, string select, string keyword, string filter, string search);
         ReadResponse<dynamic> ReadMaterialsByPRMasterItemIds(int page, int size, string order, string select, string keyword, string filter, string search, string prmasteritemids);
         Task<CostCalculationGarment> ReadByRO(string ro);
+        Task<List<CostCalculationGarment>> ReadByROs(List<string> ros);
     }
 }
