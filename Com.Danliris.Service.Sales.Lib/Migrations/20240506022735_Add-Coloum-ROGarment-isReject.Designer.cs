a0ba4b9c7b7c66a187c3654a145947dcb1aca888
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-05-06 16:50:21 +0700

    2024/10/Enhance/Add-Reject-ROGarment

--- Com.Danliris.Service.Sales.Lib/Migrations/20240506022735_Add-Coloum-ROGarment-isReject.Designer.cs
+++ Com.Danliris.Service.Sales.Lib/Migrations/20240506022735_Add-Coloum-ROGarment-isReject.Designer.cs
@@ -4,14 +4,16 @@ using Com.Danliris.Service.Sales.Lib;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Com.Danliris.Service.Sales.Lib.Migrations
 {
     [DbContext(typeof(SalesDbContext))]
-    partial class SalesDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240506022735_Add-Coloum-ROGarment-isReject")]
+    partial class AddColoumROGarmentisReject
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -441,6 +443,9 @@ namespace Com.Danliris.Service.Sales.Lib.Migrations
                         .HasMaxLength(255);
 
                     b.Property<string>("SizeRange")
+                        .HasMaxLength(50);
+
+                    b.Property<string>("SubconType")
                         .HasMaxLength(50);
 
                     b.Property<int>("THRId");
