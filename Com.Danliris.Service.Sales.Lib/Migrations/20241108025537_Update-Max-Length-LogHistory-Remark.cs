9a60362faec2a1bc8360e7b05595846fcc44ba40
supri0343 <63761987+supri0343@users.noreply.github.com>
2024-11-08 09:58:12 +0700

    2024/23/Enhance/Update-MaxLength-LogHistory-Remark

---
+++ Com.Danliris.Service.Sales.Lib/Migrations/20241108025537_Update-Max-Length-LogHistory-Remark.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Com.Danliris.Service.Sales.Lib.Migrations
+{
+    public partial class UpdateMaxLengthLogHistoryRemark : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Remark",
+                table: "LogHistories",
+                maxLength: 1000,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 255,
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Remark",
+                table: "LogHistories",
+                maxLength: 255,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldMaxLength: 1000,
+                oldNullable: true);
+
+        }
+    }
+}
